f97cea3894c2fb1b4d2bf0fd3d3a0b2df16d75e5
lucasbiel7 <lucasbiel7@icloud.com>
2021-07-09 18:20:52 -0300

    Adicionando controller para a gente testar o serviço

---
+++ src/main/java/com/battle/snake/controller/PingController.java
@@ -0,0 +1,18 @@
+package com.battle.snake.controller;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import jakarta.inject.Singleton;
+import lombok.RequiredArgsConstructor;
+
+@Singleton
+@Controller("ping")
+@RequiredArgsConstructor
+public class PingController {
+
+    @Get
+    public HttpResponse<String> ping(){
+        return HttpResponse.ok("Flávim javeiro.");
+    }
+}
